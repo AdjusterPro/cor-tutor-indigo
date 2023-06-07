d66cc4935a2e8008cd2789db090116d58feea275
Thomas O'Hara <thomas.ohara@adjusterpro.com>
2023-06-07 10:03:50 -0500

    added auto deploy to install script

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@
 
 cp cor-theming.py $(tutor plugins printroot)/.
 cp -r theme-templates $(tutor plugins printroot)/.
+
+tutor config save
+tutor images build openedx
+tutor local start -d
+tutor local do settheme cor-theme
